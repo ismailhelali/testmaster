b927b9116c701a450ac2c5ebb80883d351f7ae4f
ismailhelali <40539850+ismailhelali@users.noreply.github.com>
2023-04-24 10:09:44 +0000

    Create master.js

---
+++ master.js
@@ -0,0 +1,13 @@
+const products = [
+  {
+    name: 'MASTER Professionnelle en COACHING',  
+    firstDescription: 'cest un excellent master',
+    secondDescription:  'il sagit de bla bla bla ',
+    checked: false,
+    quantity: 1,
+    unit: 'kg',
+  },
+  // other products...
+];
+
+export { products };
