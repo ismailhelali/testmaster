632eab10a84884e1db4b0d001188975bd6adda72
ismailhelali <40539850+ismailhelali@users.noreply.github.com>
2023-04-24 11:22:11 +0000

    Update master.js

--- master.js
+++ master.js
@@ -2,7 +2,7 @@ const products = [
   {
     name: 'MASTER european Professionnelle en COACHING',  
     firstDescription: "cest un excellent master",
-    secondDescription:  "il s'agit de bla bla bla ",
+    secondDescription:  "Le Master en Coaching est une formation de deux ans qui permet aux étudiants d'acquérir des compétences en coaching professionnel. Les étudiants apprennent les principes de base du coaching, les différentes approches du coaching, ainsi que les compétences nécessaires pour travailler avec les clients dans un environnement professionnel. La formation inclut des cours sur la psychologie, la communication, le leadership, la gestion du changement, la gestion de carrière, la gestion de conflits, ainsi que sur les compétences entrepreneuriales. Les étudiants développent des compétences en évaluation",
   
   },
   // add other products here...
