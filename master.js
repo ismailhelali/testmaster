ac3ff15c62fe65e2019b1c76e44723ca3413938f
ismailhelali <40539850+ismailhelali@users.noreply.github.com>
2023-04-24 10:23:40 +0000

    Update master.js

--- master.js
+++ master.js
@@ -1,14 +1,13 @@
 const products = [
   {
-    name: 'MASTER Professionnelle en COACHING',  
-    firstDescription: 'Une filière MASTER Professionnelle sur une année, un weekend sur deux est proposée :',
-    secondDescription:  'Le Master en Coaching est une formation de deux ans qui permet aux étudiants d'acquérir des compétences en coaching professionnel. Les étudiants apprennent les principes de base du coaching, les différentes approches du coaching, ainsi que les compétences nécessaires pour travailler avec les clients dans un environnement professionnel.
-La formation inclut des cours sur la psychologie, la communication, le leadership, la gestion du changement, la gestion de carrière, la gestion de conflits, ainsi que sur les compétences entrepreneuriales. Les étudiants développent des compétences en évaluation et en analyse, ainsi que des compétences en résolution de problèmes et en prise de décisions.
-Les étudiants participent à des projets pratiques pour appliquer les principes théoriques à des situations réelles, ainsi qu'à des ateliers et des séminaires pour développer leurs compétences en communication, en leadership et en gestion d'équipe. Ils apprennent également à utiliser des outils tels que des questionnaires, des évaluations et des tests pour aider les clients à identifier leurs points forts et leurs faiblesses.
-Le Master en Coaching prépare les diplômés à travailler comme coachs professionnels dans une variété d'environnements, tels que des entreprises, des organisations à but non lucratif, des établissements d'enseignement et des organismes gouvernementaux. Les diplômés peuvent également créer leur propre entreprise de coaching ou poursuivre des études supérieures en psychologie ou en conseil.',
-    
-  },
-  // other products...
-];
+    name: "MASTER Professionnelle en COACHING",  
+    firstDescription: "Une filière MASTER Professionnelle sur une année, un weekend sur deux est proposée :",
+    secondDescription:  "Le Master en Coaching est une formation de deux ans qui permet aux étudiants d'acquérir des compétences en coaching professionnel. Les étudiants apprennent les principes de base du coaching, les différentes approches du coaching, ainsi que les compétences nécessaires pour travailler avec les clients dans un environnement professionnel.
 
-export { products };
+La formation inclut des cours sur la psychologie, la communication, le leadership, la gestion du changement, la gestion de carrière, la gestion de conflits, ainsi que sur les compétences entrepreneuriales. Les étudiants développent des compétences en évaluation et en analyse, ainsi que des compétences en résolution de problèmes et en prise de décisions.
+
+Les étudiants participent à des projets pratiques pour appliquer les principes théoriques à des situations réelles, ainsi qu'à des ateliers et des séminaires pour développer leurs compétences en communication, en leadership et en gestion d'équipe. Ils apprennent également à utiliser des outils tels que des questionnaires, des évaluations et des tests pour aider les clients à identifier leurs points forts et leurs faiblesses.
+
+Le Master en Coaching prépare les diplômés à travailler comme coachs professionnels dans une variété d'environnements, tels que des entreprises, des organisations à but non lucratif, des établissements d'enseignement et des organismes gouvernementaux. Les diplômés peuvent également créer leur propre entreprise de coaching ou poursuivre des études supérieures en psychologie ou en conseil.",
+  }
+];
