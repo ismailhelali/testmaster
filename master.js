02d37e029756c27ed700bb0c893fb275c148da0d
ismailhelali <40539850+ismailhelali@users.noreply.github.com>
2023-04-24 10:11:36 +0000

    Update master.js

--- master.js
+++ master.js
@@ -3,9 +3,7 @@ const products = [
     name: 'MASTER Professionnelle en COACHING',  
     firstDescription: 'cest un excellent master',
     secondDescription:  'il sagit de bla bla bla ',
-    checked: false,
-    quantity: 1,
-    unit: 'kg',
+    
   },
   // other products...
 ];
